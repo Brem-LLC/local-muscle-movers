7f639ac6e1d5fade883f637c95a07cd7c2ab5a58
Ted Martin <etmartinkazoo@gmail.com>
2023-10-03 16:41:06 -0400

    Cloud integration

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -2,11 +2,11 @@ import { config, fields, singleton, collection } from '@keystatic/core'
 
 export default config({
 	storage: {
-		kind: 'local',
-  //   kind: 'cloud',
-  // },
-  // cloud: {
-  //   project: 'local-muscle-movers/local-muscle-movers',
+		// kind: 'local',
+     kind: 'cloud',
+   },
+   cloud: {
+     project: 'local-muscle-movers/local-muscle-movers',
   },
 	singletons: {
 		general: singleton({
