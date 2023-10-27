8ee84d305d98857066e6d38bf2ec82328b281070
Ted Martin <etmartinkazoo@gmail.com>
2023-10-27 08:31:09 -0400

    Updated Keystatic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
 import netlify from "@astrojs/netlify/functions";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
@@ -13,7 +14,7 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), react(), partytown(), sitemap(), robotsTxt()],
+  }), react(), markdoc(), keystatic(), partytown(), sitemap(), robotsTxt()],
   output: 'hybrid',
   adapter: netlify()
 });
