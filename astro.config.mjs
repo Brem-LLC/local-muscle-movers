e2e6795b3e0d693c88f80154ae4e4171a1f3b656
Ted Martin <etmartinkazoo@gmail.com>
2023-10-27 15:00:24 -0400

    Updated Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import react from "@astrojs/react";
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import netlify from "@astrojs/netlify/functions";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
 
 
 // https://astro.build/config
@@ -15,7 +12,7 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), react(), markdoc(), keystatic(), partytown(), sitemap(), robotsTxt()],
+  }), react(), markdoc(), keystatic()],
   output: 'hybrid',
   adapter: netlify()
 });
