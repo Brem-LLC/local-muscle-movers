8686103239a370e0d4dca2956caa9678593fe93d
Ted Martin <etmartinkazoo@gmail.com>
2023-10-27 08:36:54 -0400

    More keystatic updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import markdoc from '@astrojs/markdoc'
-import keystatic from '@keystatic/astro'
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 import netlify from "@astrojs/netlify/functions";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+
 
 // https://astro.build/config
 export default defineConfig({
--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -2,11 +2,11 @@ import { config, fields, singleton, collection } from '@keystatic/core'
 
 export default config({
 	storage: {
-		kind: 'cloud',
-	},
-	cloud: {
-		project: 'local-muscle-movers/local-muscle-movers',
-	},
+    kind: 'cloud',
+  },
+  cloud: {
+    project: 'local-muscle-movers/local-muscle-movers',
+  },
 	singletons: {
 		general: singleton({
 			label: 'General Information',
