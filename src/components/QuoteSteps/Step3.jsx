25f5b38432788e6ecc7fa7b9871723feb92f951a
Joshua Bischoff <joshuabischoff1@gmail.com>
2024-01-05 12:16:44 -0500

    rearranged content to match that of current UI

--- src/components/QuoteSteps/Step3.jsx
+++ src/components/QuoteSteps/Step3.jsx
@@ -37,8 +37,8 @@ export default function Step3({ register, isValid, setValue, step, setStep, setM
     }
 
     return (
-        <div className='flex flex-col gap-5 bg-[#f5f7f2] p-12 rounded-lg'>
-            <h2 className='font-bold text-3xl absolute transform -translate-y-20 bg-[#f5f7f2] pt-2 px-8 rounded-md'>Service Details</h2>
+        <div className='flex flex-col gap-5 bg-[#f4dd973a] p-12 rounded-lg'>
+            <h2 className='font-bold text-3xl absolute transform -translate-y-[91px] bg-[#f4dd973a] pt-2 px-8 rounded-md'>Service Details</h2>
             <div className='flex flex-col gap-5 items-center'>
                 <div className='flex flex-col gap-5'>
                     <div>
